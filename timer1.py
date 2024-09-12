75c99a948f5086282f4228c24d072e0e54caa0b1
tw0709 <kchktw04290709@gmail.com>
2024-09-12 21:23:52 +0900

    updated

--- timer1.py
+++ timer1.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from tkinter import messagebox as msg
 import time
+import pygame
 t=Tk()
 t.geometry("720x480")
 t.title("타이머")
+pygame.mixer.init()
+alarm_sound = pygame.mixer.Sound("알람소리.mp3")
 mla=Label(t, text="시간을 입력하고\n시작 버튼을 눌러주세요!",font="맑은고딕 30")
 mla.pack()
 minentry=Entry(t,font="맑은고딕 20",width=10)
@@ -22,27 +25,27 @@ def update_timer(realsec):
         t.after(1000, update_timer, realsec - 1)  # 1초 후에 다시 호출
     else:
         mla.configure(text="타이머 종료됨.\n시간을 입력하고\n시작 버튼을 눌러주세요!")
-
+        pygame.mixer.Sound.play(alarm_sound)
 def checkandgo():
     try:
-        min_val = minentry.get()
-        if min_val == "":
-            min_val = 0
+        min = minentry.get()
+        if min == "":
+            min = 0
         else:
-            min_val = int(min_val)
+            min = int(min)
 
-        sec_val = secentry.get()
-        if sec_val == "":
-            sec_val = 0
+        sec = secentry.get()
+        if sec == "":
+            sec = 0
         else:
-            sec_val = int(sec_val)
+            sec = int(sec)
 
-        realsec = min_val * 60 + sec_val
+        realsec = min * 60 + sec
 
         if realsec > 0:
             update_timer(realsec)
         else:
-            msg.showwarning("경고", "타이머 시간이 0초 이하입니다.")
+            msg.showwarning("오류", "타이머 시간이 0초 이하입니다.")
     except Exception as e:
         msg.showerror("오류", f"오류가 발생하였습니다.\n{e}")
 startbu=Button(t, text="시작",font="맑은고딕 20",command=checkandgo)
